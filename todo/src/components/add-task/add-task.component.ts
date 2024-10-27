7682a21b6258bc2ae55f906498cbb0a8ec28779f
Development Team <devteam@Developments-MacBook-Air.local>
2024-10-27 13:01:41 -0500

    data binding activity

--- todo/src/components/add-task/add-task.component.ts
+++ todo/src/components/add-task/add-task.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
-export class AddTaskComponent {
+
+export class AddTaskComponent implements OnInit{
+numberOfTask:number=0;
+
+ngOnInit(): void {
+  
+}
+
+sendTask()
+{
+  return null
+}
 
 }
